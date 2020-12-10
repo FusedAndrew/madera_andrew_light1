d0b255145ac41ea86575cca8fa9d36205eb84476
FusedAndrew <72750640+FusedAndrew@users.noreply.github.com>
2020-12-10 13:17:48 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 while True:
     #print("Light Level: " + input.light_level())
-    if input.light_level() >= 125:
+    if input.light_level() >= 14:
         light.clear()
+    elif input.light_level() < 10:
+        light.set_all(light.rgb(255, 255, 0))
     else:
-        light.set_all(light.rgb(255, 255, 255))
+        light.set_all(light.rgb(0, 0, 255))
--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 while (true) {
     // print("Light Level: " + input.light_level())
-    if (input.lightLevel() >= 125) {
+    if (input.lightLevel() >= 14) {
         light.clear()
+    } else if (input.lightLevel() < 10) {
+        light.setAll(light.rgb(255, 255, 0))
     } else {
-        light.setAll(light.rgb(255, 255, 255))
+        light.setAll(light.rgb(0, 0, 255))
     }
     
 }
